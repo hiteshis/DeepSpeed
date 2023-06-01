6910f8a371ce87a0084ec1e09bee7f19295723b0
hiteshis <hi.hiteshi@gmail.com>
2023-05-31 21:41:34 -0700

    Update builder.py

--- op_builder/builder.py
+++ op_builder/builder.py
@@ -75,6 +75,8 @@ cuda_minor_mismatch_ok = {
 
 
 def assert_no_cuda_mismatch(name=""):
+    if OpBuilder.is_rocm_pytorch():
+        return True
     cuda_major, cuda_minor = installed_cuda_version(name)
     sys_cuda_version = f'{cuda_major}.{cuda_minor}'
     torch_cuda_version = ".".join(torch.version.cuda.split('.')[:2])
